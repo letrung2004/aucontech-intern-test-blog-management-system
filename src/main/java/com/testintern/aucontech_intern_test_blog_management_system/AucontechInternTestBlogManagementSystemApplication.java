eed2b84a71f5bd647ca516c313f1829ac6721778
le trung <lequoctrunggg@gmail.com>
2025-09-12 15:47:48 +0700

    first commit

---
+++ src/main/java/com/testintern/aucontech_intern_test_blog_management_system/AucontechInternTestBlogManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.testintern.aucontech_intern_test_blog_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucontechInternTestBlogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AucontechInternTestBlogManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testintern/aucontech_intern_test_blog_management_system/AucontechInternTestBlogManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testintern.aucontech_intern_test_blog_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucontechInternTestBlogManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
